f9ec01970918f8cce17dd81e4050c91fb43be0e6
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2020-02-20 15:50:45 +0100

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@
 export { Message } from './models/message.model'
 // Interfaces
 export { PlaylistItem } from './interfaces/playlist-item.interface'
-export { PlaylistItemSubmissionRequest } from './interfaces/playlist-item-submission-request.interface'
-export { PlaylistItemCancelRequest } from './interfaces/playlist-item-cancel-request.interface'
+export {
+    PlaylistItemCancelRequest,
+    PlaylistItemSubmissionRequest,
+    PlaylistItemIgnoreRequest,
+    PlaylistItemUnignoreRequest
+} from './interfaces/playlist-item-action.interface'
 export { Subscriber } from './interfaces/subscriber.interface'
 export { SyncPacket } from './interfaces/sync-packet.interface'
