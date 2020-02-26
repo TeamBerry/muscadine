adccfd789e9c750edbec76a602ec80afd29866f9
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2020-02-26 12:06:02 +0100

    Feedback properties on Message

--- src/models/message.model.ts
+++ src/models/message.model.ts
@@ -4,6 +4,20 @@ export class Message {
     public source: string
     public scope: string
     public time: Date
+    /**
+     * Qualifies the message. Server will send 'feedbacks'
+     *
+     * @type {('feedback' | 'message')}
+     * @memberof Message
+     */
+    public type: 'feedback' | 'message'
+    /**
+     * Qualifies the feedback of the message
+     *
+     * @type {('error' | 'success' | 'info' | 'warning')}
+     * @memberof Message
+     */
+    public feedbackType: 'error' | 'success' | 'info' | 'warning'
 
     constructor(message?: any) {
         this.author = message && message.author || null
@@ -11,5 +25,7 @@ export class Message {
         this.source = message && message.source || null
         this.scope = message && message.scope || null
         this.time = message && message.time || new Date()
+        this.type = message && message.type || 'message'
+        this.feedbackType = message && message.feedbackType || null
     }
 }
