4cd0d4c4619db976f3e49f5874cd9dc520a2444b
AngelZatch <angelzatch@gmail.com>
2020-06-10 09:59:38 +0200

    fix: export ActiveSubscriber

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,6 @@ export {
     VideoSubmissionRequest,
     PlaylistSubmissionRequest
 } from './interfaces/queue-actions.interface'
-export { Subscriber, BerryCount } from './interfaces/subscriber.interface'
+export { Subscriber, BerryCount, ActiveSubscriber } from './interfaces/subscriber.interface'
 export { SyncPacket } from './interfaces/sync-packet.interface'
 export { Role, Permission, ACLConfig } from './interfaces/acl.interface'
