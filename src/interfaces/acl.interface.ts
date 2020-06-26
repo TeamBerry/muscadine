3e48233e357f183420f254b4f4d6857233f090b8
AngelZatch <angelzatch@gmail.com>
2020-06-26 14:46:03 +0200

    feat: RoleChangeRequest & 'admin' as Role value

--- src/interfaces/acl.interface.ts
+++ src/interfaces/acl.interface.ts
@@ -1,9 +1,35 @@
+import { BoxScope } from "./box-scope.interface"
+
 export type Permission = 'addVideo' | 'removeVideo' | 'forceNext' | 'forcePlay' | 'skipVideo' | 'editBox' | 'promoteVIP' | 'demoteVIP'
 
-export type Role = 'moderator' | 'vip' | 'simple'
+export type Role = 'admin' | 'moderator' | 'vip' | 'simple'
 
 export interface ACLConfig {
     moderator: Array<Permission>
     vip: Array<Permission>
     simple: Array<Permission>
+}
+
+export interface RoleChangeRequest {
+    /**
+     * The requester
+     *
+     * @type {string}
+     * @memberof RoleChangeRequest
+     */
+    source: string,
+    /**
+     * Who and where to apply the request
+     *
+     * @type {BoxScope}
+     * @memberof RoleChangeRequest
+     */
+    scope: BoxScope
+    /**
+     * The role to give to the target (source.userToken)
+     *
+     * @type {Role}
+     * @memberof RoleChangeRequest
+     */
+    role: Role,
 }
